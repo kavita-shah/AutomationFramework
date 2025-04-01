bbb11b38e36db7f7bf5c2dd8112b262df1566d8b
SRIKANTH <SRIKANTH@DESKTOP-G13II8U>
2025-04-01 12:12:41 +0530

    hello this is Puli-Srikanth

--- AutomationProject/src/test/java/ContactTests/ToCreateContact.java
+++ AutomationProject/src/test/java/ContactTests/ToCreateContact.java
@@ -3,7 +3,8 @@ package ContactTests;
 public class ToCreateContact {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("contact created successfully");
 
 	}
 
--- AutomationProject/src/test/java/OrgTests/ToCreateOrg.java
+++ AutomationProject/src/test/java/OrgTests/ToCreateOrg.java
@@ -3,8 +3,8 @@ package OrgTests;
 public class ToCreateOrg {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Organization created successfully");
 	}
 
 }
